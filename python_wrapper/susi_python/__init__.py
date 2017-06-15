8cdd576e4955966b0afbc7fccecfdf91b26f6373
Pranjal Paliwal <paliwal.pranjal83@gmail.com>
2017-06-16 00:33:49 +0530

    direct response parse from JSON method added (#24)

--- python_wrapper/susi_python/__init__.py
+++ python_wrapper/susi_python/__init__.py
@@ -1,6 +1,7 @@
 from .main import use_api_endpoint
 from .main import query
 from .main import ask
+from .main import answer_from_json
 from .main import sign_in
 from .main import sign_up
 from .main import forgot_password
