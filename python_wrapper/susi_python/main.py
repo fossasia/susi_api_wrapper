bad6b85af52a8d2b83988bd648fa7f9afe74baf9
Sanskar <sansyrox@gmail.com>
2018-05-24 20:26:16 +0530

    Added Test to check if server is down in between the conversation

--- python_wrapper/susi_python/main.py
+++ python_wrapper/susi_python/main.py
@@ -51,8 +51,18 @@ def query(query_string):
         params['latitude'] = location['latitude']
         params['longitude'] = location['longitude']
 
+    global api_endpoint
     chat_url = api_endpoint + "/susi/chat.json"
-    api_response = requests.get(chat_url, params)
+    try:
+        api_response = requests.get(chat_url, params)
+    except requests.exceptions.ConnectionError:
+        if api_endpoint == 'http://localhost:4000' | api_endpoint == 'https://localhost:4000':
+            api_endpoint = 'https://api.susi.ai'
+            api_response = requests.get(chat_url, params)
+        elif api_endpoint == 'http://api.susi.ai' | api_endpoint == 'https://api.susi.ai':
+            api_endpoint = 'http://localhost:4000'
+            api_response = requests.get(chat_url, params)
+
     response_json = api_response.json()
     parsed_res = get_query_response(response_json)
     return parsed_res
